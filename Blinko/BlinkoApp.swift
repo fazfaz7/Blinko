8e7325ced76f33e5459678b4d61cee33c591dc44
fazfaz7 <adrian.faz123@gmail.com>
2025-05-16 16:27:23 +0200

    Initial Commit

---
+++ Blinko/BlinkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkoApp.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BlinkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
