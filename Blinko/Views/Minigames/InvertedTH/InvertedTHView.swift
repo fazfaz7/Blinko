3618431e2a11b3861350983bf7017df74cad4e45
fazfaz7 <adrian.faz123@gmail.com>
2025-05-17 18:48:06 +0200

    Creation of the Inverted Treasure Hunt View. For now, the minigame starts with one of the objects to be found and the other cards in black. Once the user finds the object, the card becomes green and the next card shows. When the 4 objects are found a beautiful picture shows up. First version of Inverted Treasure Hunt.

---
+++ Blinko/Views/Minigames/InvertedTH/InvertedTHView.swift
@@ -0,0 +1,18 @@
+//
+//  InvertedTHView.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 17/05/25.
+//
+
+import SwiftUI
+
+struct InvertedTHView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvertedTHView()
+}
