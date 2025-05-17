fc60e099b9b907574d45c5d8392387bcf5dac055
fazfaz7 <adrian.faz123@gmail.com>
2025-05-17 16:02:17 +0200

    Test Commit Faz!

--- Blinko/ContentView.swift
+++ Blinko/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Vincenzo has been here")
+            Text("Faz has been here")
         }
         .padding()
     }
