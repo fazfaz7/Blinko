130e8f62b091d73bcd88d6d712952b1a6f51adeb
fazfaz7 <adrian.faz123@gmail.com>
2025-05-23 12:13:08 +0200

    New Navigation & Objects!

---
+++ Blinko/Models/LevelProgress.swift
@@ -0,0 +1,8 @@
+//
+//  LevelProgress.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 22/05/25.
+//
+
+import Foundation
---
+++ Blinko/Views/Levels/MinigameSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MinigameSelectionView.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 22/05/25.
+//
+
+import SwiftUI
+
+struct MinigameSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MinigameSelectionView()
+}
