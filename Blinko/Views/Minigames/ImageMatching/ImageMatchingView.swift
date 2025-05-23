9c7f207b474015690d5e4a69c05e12b021b9c5fc
Vincenzo-G <vincenzogerelli@gmail.com>
2025-05-23 10:26:48 +0200

    Commented the view

--- Blinko/Views/Minigames/ImageMatching/ImageMatchingView.swift
+++ Blinko/Views/Minigames/ImageMatching/ImageMatchingView.swift
@@ -16,15 +16,31 @@ struct ImageMatchingView: View {
     let words: [VocabularyWord]
     let colors: [Color] = [.orangeBlinko, .pinkBlinko, .lilaBlinko, .purpleBlinko]
     
+    // Track the remaining words still in the game
     @State private var remainingWords: [VocabularyWord]
+    
+    // The word the user has to tap
     @State private var targetWord: VocabularyWord?
+    
+    // Used by the shake animation
     @State private var wrongTapTriggers: [UUID: CGFloat] = [:]
+    
+    // Map the colors to the index, so they don't shift
     @State private var colorMap: [UUID: Color] = [:]
+    
+    // Show Blinko animation after the complete level
     @State private var showMascotJump = false
+    
+    // Useful for Blinko jumping
     @State private var mascotOffset: CGFloat = 0
+    
+    // Useful for Blinko jumping?
     @State private var isJumping = false
-    @StateObject private var TextToSpeachViewModel = TextToSpeechViewModel(
+    
+    // TextToSpeech viewModel to prompt words
+    @StateObject private var SpeechViewModel = TextToSpeechViewModel(
         textToSpeechService: TextToSpeechService())
+    
     // Closure used to go to the next minigame. (Image Matching)
     var onNext: () -> Void
     
@@ -47,13 +63,15 @@ struct ImageMatchingView: View {
         VStack(spacing: 30) {
             Spacer()
             
+            // List the cards
             HStack(spacing: 30) {
                 ForEach(remainingWords, id: \.id) { word in
                     let color = colorMap[word.id] ?? .gray
                     CardView(cardSize: 240, imageName: word.imageName, withLabel: true, label: word.baseWord, cardColor: color)
                         .modifier(Shake(animatableData: wrongTapTriggers[word.id, default: 0]))
                         .onTapGesture {
-                            TextToSpeachViewModel.speak(text: word.baseWord, language: "English")
+                            // Every time user tap a card it will trigger audio and check if it's the right card
+                            SpeechViewModel.speak(text: word.baseWord, language: "English")
                             checkAnswer(selected: word)
                         }
                 }
@@ -61,13 +79,14 @@ struct ImageMatchingView: View {
             }
             .padding(.horizontal)
             
+            // Button to show the target word
             if let word = targetWord {
                 Button(action: {
-                    TextToSpeachViewModel.speak(text: word.baseWord, language: "English")
+                    SpeechViewModel.speak(text: word.baseWord, language: "English")
                 }) {
                     HStack(spacing: 8) {
-                        //Text(word.baseWord)
-                        //    .font(.custom("Baloo2-Bold", size: 70))
+                        Text(word.baseWord)
+                            .font(.custom("Baloo2-Bold", size: 70))
                         Image(systemName: "speaker.wave.2.fill")
                             .font(.system(size: 90))
                     }
@@ -81,8 +100,8 @@ struct ImageMatchingView: View {
                 
             }
             
+            // Blinko animation after the game has ended
             if(showMascotJump){
-                
                     Image("blinko2")
                         .resizable()
                         .scaledToFit()
@@ -116,16 +135,18 @@ struct ImageMatchingView: View {
         }
     }
     
+    // Picks a new target from the remaining words, and prompt the audio
     func pickNewTarget() {
         if !remainingWords.isEmpty {
             targetWord = remainingWords.randomElement()
             if let word = targetWord {
-                TextToSpeachViewModel.speak(text: word.baseWord, language: "English")            }
+                SpeechViewModel.speak(text: word.baseWord, language: "English")            }
         } else {
             targetWord = nil
         }
     }
     
+    // Check if the answer user selected is right
     func checkAnswer(selected: VocabularyWord) {
         if selected == targetWord {
             withAnimation{
@@ -174,5 +195,5 @@ struct ImageMatchingView: View {
 }
 
 #Preview {
-   // ImageMatchingView(words: level1.words)
+    ImageMatchingView(words: level1.words, onNext: {} )
 }
