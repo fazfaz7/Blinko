65e15e0267d79e4106cd601aee757d781f81beb0
fazfaz7 <adrian.faz123@gmail.com>
2025-05-24 21:04:03 +0200

    Attempt to do the effects of the levels design (FAILED :((()

---
+++ Blinko/Views/Levels/LevelsDesignView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelsDesignView.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 23/05/25.
+//
+
+import SwiftUI
+
+struct LevelsDesignView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelsDesignView()
+}
---
+++ Blinko/Views/Levels/ParallaxView.swift
@@ -0,0 +1,18 @@
+//
+//  ParallaxView.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 24/05/25.
+//
+
+import SwiftUI
+
+struct ParallaxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParallaxView()
+}
