01e5d43f32c4d110127da75fbff523931ee019f5
fazfaz7 <adrian.faz123@gmail.com>
2025-05-26 19:16:23 +0200

    Added the minigames planets and first version of the MinigamesView. With the final design of the planets.

---
+++ Blinko/Views/Levels/ChooseMinigameView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseMinigameView.swift
+//  Blinko
+//
+//  Created by Adrian Emmanuel Faz Mercado on 26/05/25.
+//
+
+import SwiftUI
+
+struct ChooseMinigameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseMinigameView()
+}
